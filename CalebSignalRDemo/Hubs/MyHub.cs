04602769e79c73282c8cdb5cb14cc6cc1ef2aa7a
xtijohn <50695306+xtijohn@users.noreply.github.com>
2020-03-20 14:59:32 +0200

    Changes
    Changes in code.
    1)Attributes explanation
    2)SignalR Log.
    3)Get the server's DateTime

--- CalebSignalRDemo/Hubs/MyHub.cs
+++ CalebSignalRDemo/Hubs/MyHub.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace CalebSignalRDemo.Hubs
 {
+    //[HubName("myHub")] //gives a hub name. That name is the name i use in the .js when i want to call the hub
     public class MyHub : Hub
     {
+        //[HubMethodName("announceToEverybody")] // rename a method on the client side. so everytime i want to call the Announce() in .js,
+        //// i have to call the announceToEverybody() 
         public void Announce(string message)
         {
             //every single client has a function named Announced.
             Clients.All.Announce(message);
+        }
+        //Return server's date-time to the specific client who called the method.
+        //1st way
+        //public void GetServerDateTime()
+        //{
+        //    Clients.Caller.DisplayDateTime();
+        //}
+        //2nd way
+        public DateTime GetServerDateTime()
+        {
+            return DateTime.Now;
         }
     }
 }
--- CalebSignalRDemo/Scripts/SignalR.js
+++ CalebSignalRDemo/Scripts/SignalR.js
@@ -2,10 +2,18 @@
 //Immediately Invoked Function Expression (IIFE)
 //we group everything under a function to specify the scope of each functionality.
 (function () {
+    $("#click-me").on("click", function () {
+        myHub.server.getServerDateTime()
+            .done(function (data) { writeToPage(data); })
+            .fail(function (e) { writeToPage(e); });
+    });
+
     var myHub = $.connection.myHub;
     $.connection.hub.start()
         .done(function () {
+            $.connection.hub.logging = true;
             writeToPage("IT WORKED!" + "<br/>");
+            $.connection.hub.log("Connected");
             myHub.server.announce("Connected!!");
         })
         .fail(function () {
